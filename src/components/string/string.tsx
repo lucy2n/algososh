9f7ca161ba34eb4f1413870d81299a058217145e
lucy2n <lyusianaumenko@yandex.by>
2023-10-06 12:57:18 +0300

    feat: add basic layot for string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import styles from './string.module.css'
+import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
+  const [input, setInput] = useState('');
+  const [arr, setArr] = useState<Array<string>>([])
+  const addWord = () => {
+    setArr(input.split(''))
+    console.log(arr)
+  }
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles.main}> 
+        <Input value={input} onChange={(e) => setInput(e.currentTarget.value)} isLimitText={true} maxLength={11} extraClass={styles.mr} />
+        <Button text="Развернуть" onClick={addWord}/>
+      </div>
+      <div className={styles.letters}>
+        {
+          arr.map((item) => 
+            <Circle extraClass={styles.letter} letter={item}></Circle>
+          ) 
+        }
+      </div>
     </SolutionLayout>
   );
 };
