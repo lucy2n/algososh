ffa22de0575c0b4923654638fe5b8b4d9705eef1
lucy2n <lyusianaumenko@yandex.by>
2023-10-07 13:38:16 +0300

    feat: finish fibonacci page

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,47 +1,54 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import styles from './fibonacci.module.css'
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
 export const FibonacciPage: React.FC = () => {
   const [input, setInput] = useState<string>('');
   const [arr, setArr] = useState<Array<string>>([])
+  const [buttonPressed, setButtonPressed] = useState<boolean>(false)
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
 
-  const fibIterative = (n: number) => {
+  async function sleep(millis: number) {
+    return new Promise((resolve) => setTimeout(resolve, millis));
+  }
+
+  const getFibonacciNumbers = async (n: number) => {
     if(1 <= n && n <= 19)  {
+      setArr([]);
       let tempArr: number[] = [0, 1];
-      for (let i = 2; i <= n + 1; i++){
-        tempArr.push(tempArr[i - 2] + tempArr[i - 1])
+      setArr(['1'])
+      await sleep(SHORT_DELAY_IN_MS);
+      for (let i = 2; i <= n + 1; i++) {
+        tempArr.push(tempArr[i - 2] + tempArr[i - 1])   
+        setArr(tempArr.slice(1).map((item) => item.toString()))
+        await sleep(SHORT_DELAY_IN_MS);
       }
-      tempArr.shift()
-      setArr(tempArr.map((item) => item.toString()))
     } else {
-      return []
+      setArr([])
     }
+    setButtonPressed(false)
   } 
 
   const calculate = () => {
-    fibIterative(+input)
+    setButtonPressed(true)
+    getFibonacciNumbers(+input)
   }
   
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
       <div className={styles.main}> 
         <Input type='!text' value={input} onChange={onChange} isLimitText={true} max={19} extraClass={styles.mr} />
-        <Button text="Рассчитать" onClick={calculate}/>
+        <Button text="Рассчитать" onClick={calculate} isLoader={buttonPressed} disabled={buttonPressed}/>
       </div>
       <div className={styles.letters}>
-        {
-          arr.map((item, index) => 
-            <Circle index={index} letter={item}></Circle>
-          )
-        }
+        { arr.map((item, index) => <Circle index={index} letter={item}/>) }
       </div>
     </SolutionLayout>
   );
