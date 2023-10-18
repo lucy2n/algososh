8be2695a7da3b82105c35e6fd242b51575b7811a
lucy2n <lyusianaumenko@yandex.by>
2023-10-18 15:57:38 +0300

    feat: add tests for circle

--- src/components/ui/button/button.test.js
+++ src/components/ui/button/button.test.js
@@ -3,27 +3,27 @@ import React from "react";
 import { render, screen, fireEvent } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 
-it('Кнопка без текста рендерится без ошибок', () => {
+it('The button without text is rendered without errors', () => {
     const buttonWithoutText = renderer.create(<Button />)
     expect(buttonWithoutText).toMatchSnapshot();
 }); 
 
-it('Кнопка c текстом рендерится без ошибок', () => {
+it('The button with text is rendered without errors', () => {
     const buttonWithText = renderer.create(<Button text="test"/>)
     expect(buttonWithText).toMatchSnapshot();
 }); 
 
-it('Заблокированная кнопка рендерится без ошибок', () => {
+it('The locked button is rendered without errors', () => {
     const buttonDisabled = renderer.create(<Button disabled={true} />)
     expect(buttonDisabled).toMatchSnapshot();
 }); 
 
-it('Kнопкa с индикацией загрузки рендерится без ошибок', () => {
+it('The button with load indication is rendered without errors', () => {
     const buttonIsLoader = renderer.create(<Button isLoader={true} />)
     expect(buttonIsLoader).toMatchSnapshot();
 }); 
 
-it('Колбек работает корректно', () => {
+it('Callback is working correctly', () => {
     const onClick = jest.fn();
     render(<Button text="test button" onClick={onClick}/>)
     const button = screen.getByText("test button");
