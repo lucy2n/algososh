82d8a1db37080880c3921b26fe77afe5e69314c8
lucy2n <lyusianaumenko@yandex.by>
2023-10-25 23:31:52 +0300

    feat: add all tests for string

--- src/components/string/string.test.js
+++ src/components/string/string.test.js
@@ -27,4 +27,61 @@ describe('String Commponent', () => {
             expect(circles[3]).toHaveTextContent('1');
         }, { timeout: DELAY_IN_MS * 3 } )
     }); 
+
+    it('A string with an odd number of characters is expanded correctly', async () => {
+        render(
+            <BrowserRouter>
+                <StringComponent />
+            </BrowserRouter>
+        );
+        const inputElement = screen.getByTestId('input');
+        fireEvent.change(inputElement, { target: {value: '123'} })
+        expect(inputElement.value).toBe('123')
+
+        const buttonElement = screen.getByTestId('reverse_button');
+        fireEvent.click(buttonElement);
+
+        await waitFor(() => {
+            const circles = screen.getAllByTestId('circle');
+            expect(circles[0]).toHaveTextContent('3');
+            expect(circles[1]).toHaveTextContent('2');
+            expect(circles[2]).toHaveTextContent('1');
+        }, { timeout: DELAY_IN_MS * 2 } )
+    });  
+
+    it('A string with a single number of characters is expanded correctly', async () => {
+        render(
+            <BrowserRouter>
+                <StringComponent />
+            </BrowserRouter>
+        );
+        const inputElement = screen.getByTestId('input');
+        fireEvent.change(inputElement, { target: {value: '1'} })
+        expect(inputElement.value).toBe('1')
+
+        const buttonElement = screen.getByTestId('reverse_button');
+        fireEvent.click(buttonElement);
+
+        await waitFor(() => {
+            const circles = screen.getAllByTestId('circle');
+            expect(circles[0]).toHaveTextContent('1');
+        }, { timeout: DELAY_IN_MS * 2 } )
+    }); 
+
+    it('A string with an empty string of characters is expanded correctly', async () => {
+        render(
+            <BrowserRouter>
+                <StringComponent />
+            </BrowserRouter>
+        );
+        const inputElement = screen.getByTestId('input');
+        fireEvent.change(inputElement, { target: {value: ''} })
+        expect(inputElement.value).toBe('')
+
+        const buttonElement = screen.getByTestId('reverse_button');
+        expect(buttonElement).toBeDisabled();
+
+    }); 
+
 });
+
